4618e62fd0d68c37a2901d469de80d9210b35ee8
jerrybowang <jerrybowang@icloud.com>
2024-02-29 20:08:53 -0800

    finish bare bones: web app and prediction model

--- Container/main.py
+++ Container/main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, send_file, render_template, jsonify
+import prediction
+from flask import Flask, request, send_file, render_template, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -22,14 +23,16 @@ def upload_file():
             # Construct URL for displaying image
             image_url = f"/display/{file.filename}"
             # Return HTML response with image URL
-            return f"<img src='{image_url}'><br><button onclick=\"window.location.href='/delete/{file.filename}'\">Delete Image</button>"
+            return render_template("show_result.html", image_url=image_url, class_name=prediction.predict(file_path), filename=file.filename)
     elif request.method == "GET":
         return render_template("upload.html")  # Assuming you have an HTML template for the upload form
+
 
 # Route for displaying image
 @app.route("/display/<filename>")
 def display_image(filename):
     return send_file(os.path.join("uploads", filename), mimetype='image/png')
+
 
 # Route for deleting uploaded file
 @app.route("/delete/<filename>", methods=["GET"])
@@ -37,15 +40,15 @@ def delete_file(filename):
     file_path = os.path.join("uploads", filename)
     if os.path.exists(file_path):
         os.remove(file_path)
-        return jsonify({"message": f"File {filename} deleted successfully"})
+        # redirect
+        return render_template('delete_confirmation.html', filename=filename, redirect_url=url_for('upload_file'))
     else:
+        # if file not found
         return jsonify({"message": f"File {filename} does not exist"})
 
 @app.route("/")
 def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return render_template("index.html", upload_url=url_for("upload_file"))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
